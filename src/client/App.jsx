a03bc2b0bc909b0f472fb3f282500d4e08870045
joycemap <57893220+joycemap@users.noreply.github.com>
2020-02-27 16:30:31 +0800

    Added input box and Add button

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,12 +2,32 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+  constructor(){
+    super()
+  
+    this.state = {
+      word : ""
+    }
+  }
+  
+  changeHandler(event){
+    this.setState({word:event.target.value});
+    console.log("change", event.target.value);
+  }
+
+  clickHandler(){
+    console.log("clicking");
+  }
   render() {
-    return (
-      <div>
-        Welcome.
-      </div>
-    );
+      console.log("rendering");
+      return (
+        <div className="item">
+          {this.state.word}
+          <input onChange={(event)=>{this.changeHandler(event);}}/>
+        
+<button onClick={()=>{this.clickHandler()}}>ADD</button>
+</div>
+      );
   }
 }
 
